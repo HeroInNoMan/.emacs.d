d10ff37d061e9dc53be013e3403f4ef8170e37c6
Arthur Léothaud <arthur@leothaud.eu>
2015-12-18 12:36:21 +0100

    remove arrow key search (never used)

--- init.el
+++ init.el
@@ -548,16 +548,6 @@ Results are reported in a compilation buffer."
 (require 're-builder)
 (setq reb-re-syntax 'string) ;; syntax used in the re-buidler
 
-
-;; i-search with arrow keys
-(progn
-  ;; set arrow keys in isearch. left/right is backward/forward, up/down is history. press Return to exit
-  (define-key isearch-mode-map (kbd "<up>") 'isearch-ring-retreat )
-  (define-key isearch-mode-map (kbd "<down>") 'isearch-ring-advance )
-  (define-key isearch-mode-map (kbd "<left>") 'isearch-repeat-backward) ; single key, useful
-  (define-key isearch-mode-map (kbd "<right>") 'isearch-repeat-forward) ; single key, useful
-  )
-
 ;; swipe instead of regular search
 (global-set-key (kbd "C-S-s") 'swiper-helm)
 
