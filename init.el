a971a9f26a3d2e247edfaf668804e7f6b9b0a9b3
Arthur Léothaud <arthur@leothaud.eu>
2015-12-23 13:15:21 +0100

    fix org-mode-map call (require org explicitly)

--- init.el
+++ init.el
@@ -892,6 +892,7 @@ _mx_: xml
 ;; (setq jedi:complete-on-dot t) ;; optional
 
 ;; ORG-MODE
+(require 'org)
 (global-set-key (kbd "\C-c l") 'org-store-link)
 (global-set-key (kbd "\C-c a") 'org-agenda)
 (global-set-key (kbd "\C-c b") 'org-iswitchb)
