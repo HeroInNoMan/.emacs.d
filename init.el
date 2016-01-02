5896027cc0bc76bf22fb023f9e1d03014f18cb78
Arthur Léothaud <arthur@leothaud.eu>
2016-01-02 16:06:05 +0100

    fixup helm config

--- init.el
+++ init.el
@@ -979,6 +979,7 @@ _mx_: xml
  '(custom-safe-themes
    (quote
 	("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+ '(helm-external-programs-associations (quote (("mp3" . "audacious"))))
  '(magit-fetch-arguments (quote ("--prune")))
  '(magit-log-arguments (quote ("--graph" "--color" "--decorate" "--date-order"))))
 (custom-set-faces
