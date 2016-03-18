6df92f2e5b6f7cb1aa9e74580138a437e3dd96fc
Arthur Léothaud <arthur@leothaud.eu>
2016-03-18 15:02:53 +0100

    improve god-mode switching behaviour

--- init.el
+++ init.el
@@ -294,26 +294,21 @@
 (global-set-key (kbd "C-c g") 'god-mode-all)
 (global-set-key (kbd "<f8>") 'god-mode-all)
 (defun my-update-cursor ()
-  (if god-local-mode
+  (cond
+   (god-local-mode
+    (progn
       (set-cursor-color "red")
-    (set-cursor-color "yellow")))
+      (set-face-background 'mode-line "brown")))
+   (t
+    (progn
+      (set-cursor-color "yellow")
+      (set-face-background 'mode-line "#0a2832")))))
 
 (add-hook 'god-mode-enabled-hook 'my-update-cursor)
 (add-hook 'god-mode-disabled-hook 'my-update-cursor)
 (add-hook 'window-configuration-change-hook 'my-update-cursor)
 (define-key god-local-mode-map (kbd "z") 'repeat)
-(define-key god-local-mode-map (kbd ".") 'repeat)
 (define-key god-local-mode-map (kbd "i") 'god-mode-all)
-
-(defun c/god-mode-update-cursor ()
-  (let ((limited-colors-p (> 257 (length (defined-colors)))))
-    (cond (god-mode (progn
-                      (set-face-background 'mode-line (if limited-colors-p "white" "#e9e2cb"))
-                      (set-face-background 'mode-line-inactive (if limited-colors-p "white" "#e9e2cb"))))
-          (t (progn
-               (set-face-background 'mode-line (if limited-colors-p "black" "#0a2832"))
-               (set-face-background 'mode-line-inactive (if limited-colors-p "black" "#0a2832")))))))
-(define-key god-local-mode-map (kbd "i") 'god-local-mode)
 (define-key god-local-mode-map (kbd ".") 'repeat)
 (add-to-list 'god-exempt-major-modes 'ibuffer-mode)
 
