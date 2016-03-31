4f421df9dd6840392a7c10d24daf74349911d2ad
Arthur Léothaud <arthur@leothaud.eu>
2016-03-31 18:37:29 +0200

    add linum activation while using command goto-line

--- init.el
+++ init.el
@@ -619,6 +619,18 @@ _mx_: xml
 (setq line-number-mode t
       column-number-mode t)
 
+;; don’t display linum except while goto-line
+(global-set-key [remap goto-line] 'goto-line-with-feedback)
+
+(defun goto-line-with-feedback ()
+  "Show line numbers temporarily, while prompting for the line number input"
+  (interactive)
+  (unwind-protect
+      (progn
+        (linum-mode 1)
+        (goto-line (read-number "Goto line: ")))
+    (linum-mode -1)))
+
 ;; join lines below onto current line
 (global-set-key (kbd "M-j")
 				(lambda ()
