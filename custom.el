d4c029808f9763ec02091f5cf9851a38e7839d30
Arthur Léothaud <arthur@leothaud.eu>
2016-04-15 17:56:24 +0200

    format custom.el

--- custom.el
+++ custom.el
@@ -5,19 +5,20 @@
  ;; If there is more than one, they won't work right.
  '(avy-keys
    (quote
-	(97 117 105 101 98 233 112 111 116 115 114 110 109 118 100 108 106 99 113 103 104 102)))
+    (97 117 105 101 98 233 112 111 116 115 114 110 109 118 100 108 106 99 113 103 104 102)))
  '(custom-safe-themes
    (quote
-	("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
  '(helm-external-programs-associations (quote (("mp3" . "audacious"))))
  '(magit-fetch-arguments (quote ("--prune")))
  '(magit-log-arguments
    (quote
-	("--graph" "--color" "--decorate" "--date-order" "-n100")))
+    ("--graph" "--color" "--decorate" "--date-order" "-n100")))
  '(magit-stash-arguments (quote ("--include-untracked")))
  '(org-agenda-files
    (quote
-	("~/Terminalcity/.emacs.d/gcal.org" "/sshx:polopeche:/home/duncan/Terminalcity/Todo/arthur.org"))))
+    ("~/Terminalcity/.emacs.d/gcal.org" "/sshx:polopeche:/home/duncan/Terminalcity/Todo/arthur.org")))
+ '(sml/use-projectile-p (quote before-prefixes)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
