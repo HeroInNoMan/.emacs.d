6af1040d4d48abc8f15468b5ab6faa4bad19e8a9
Arthur Léothaud <arthur@leothaud.eu>
2016-06-01 10:49:57 +0200

    add function to improve fill-paragraph (M-q)

--- init.el
+++ init.el
@@ -115,6 +115,19 @@
 ;; Lines should be 80 characters wide, not 70
 (setq-default fill-column 80)
 
+;; fill or unfill paragraph (M-q as a toggle)
+(defun endless/fill-or-unfill ()
+  "Like `fill-paragraph', but unfill if used twice."
+  (interactive)
+  (let ((fill-column
+         (if (eq last-command 'endless/fill-or-unfill)
+             (progn (setq this-command nil)
+                    (point-max))
+           fill-column)))
+    (call-interactively #'fill-paragraph)))
+(global-set-key [remap fill-paragraph]
+                #'endless/fill-or-unfill)
+
 ;; Don't be so stingy on the memory, we have lots now. It's the distant future.
 (setq gc-cons-threshold 20000000)
 
