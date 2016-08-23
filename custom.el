766275495cdbaf141dec9a3c0a2459c615c06067
Arthur Léothaud <arthur@leothaud.eu>
2016-08-23 15:19:43 +0200

    update custom.el

--- custom.el
+++ custom.el
@@ -10,8 +10,7 @@
    (quote
     ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
  '(ecb-options-version "2.40")
- '(elfeed-feeds
-   my-private-feeds)
+ '(elfeed-feeds my-private-feeds)
  '(helm-external-programs-associations (quote (("mp3" . "audacious"))))
  '(magit-fetch-arguments (quote ("--prune")))
  '(magit-log-arguments
@@ -21,6 +20,9 @@
  '(org-agenda-files
    (quote
     ("~/Terminalcity/.emacs.d/gcal.org" "/sshx:polopeche:/home/duncan/Terminalcity/Todo/arthur.org")))
+ '(package-selected-packages
+   (quote
+    (ibuffer-vc ibuffer-projectile zop-to-char zoom-frm wttrin which-key wgrep web-mode volatile-highlights visual-regexp-steroids use-package-chords undo-tree tidy swiper-helm sublimity spray sml-modeline smartscan smart-mode-line smart-comment shrink-whitespace region-bindings-mode rainbow-mode rainbow-delimiters paredit origami org-bullets multiple-cursors move-text minimap magit java-snippets jade imenu-anywhere idle-highlight-mode hydra helm-projectile helm-git-grep helm-descbinds helm-ag goto-last-change god-mode gitignore-mode gitconfig-mode git-timemachine git-messenger git-gutter-fringe git-gutter+ flycheck fancy-narrow f3 expand-region engine-mode elisp-slime-nav elfeed edit-server dumb-jump dot-mode dired-narrow dired+ describe-number define-word crux color-theme char-menu calfw-gcal calfw butler browse-kill-ring better-defaults avandu aggressive-indent ace-window)))
  '(sml/use-projectile-p (quote before-prefixes)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
