b9289f59cd8d80d2c411da878734337c3b64fa8e
Arthur Léothaud <arthur@leothaud.eu>
2016-09-02 17:26:17 +0200

    add func to toggle between camelcase & underscores

--- elisp/my-functions.el
+++ elisp/my-functions.el
@@ -279,5 +279,22 @@ for the line number input"
           (insert (concat "[" s "] ")))
       (goto-char (point-min)))))
 
+(defun toggle-camelcase-underscores ()
+  "Toggle between camelcase and underscore notation for the symbol at point."
+  (interactive)
+  (save-excursion
+    (let* ((bounds (bounds-of-thing-at-point 'symbol))
+           (start (car bounds))
+           (end (cdr bounds))
+           (currently-using-underscores-p (progn (goto-char start)
+                                                 (re-search-forward "_" end t))))
+      (if currently-using-underscores-p
+          (progn
+            (upcase-initials-region start end)
+            (replace-string "_" "" nil start end)
+            (downcase-region start (1+ start)))
+        (replace-regexp "\\([A-Z]\\)" "_\\1" nil (1+ start) end)
+        (downcase-region start (cdr (bounds-of-thing-at-point 'symbol)))))))
+
 (provide 'my-functions)
 ;; misc-functions.el ends here.
