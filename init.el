361a55f0942ceca4ac6c90f280fef00cd1aa06ca
Arthur Léothaud <arthur.leothaud@viseo.com>
2016-11-25 18:03:04 +0100

    add column type for ibuffer

--- init.el
+++ init.el
@@ -737,6 +737,16 @@
 
 (use-package ibuffer-vc
   :config
+  ;; Use human readable Size column instead of original one
+  (define-ibuffer-column size-h
+    (:name "Size" :inline t)
+    (cond
+     ((> (buffer-size) 1000000) (format "%7.1fM" (/ (buffer-size) 1000000.0)))
+     ((> (buffer-size) 100000) (format "%7.0fk" (/ (buffer-size) 1000.0)))
+     ((> (buffer-size) 1000) (format "%7.1fk" (/ (buffer-size) 1000.0)))
+     (t (format "%8d" (buffer-size)))))
+
+
   (add-hook 'ibuffer-hook
             (lambda ()
               (ibuffer-vc-set-filter-groups-by-vc-root)
