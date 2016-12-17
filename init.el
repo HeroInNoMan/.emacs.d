1059259e8c2286ae6f335b9d50d2a1771929a7aa
Arthur Léothaud <hero.in.no.man@gmail.com>
2016-12-17 02:37:41 +0100

    add xml formatter

--- init.el
+++ init.el
@@ -1137,6 +1137,14 @@
          "\\.xsd\\'"
          "\\.wsdl\\'"))
 
+(defun mu-xml-format ()
+  "Format an XML buffer with `xmllint'."
+  (interactive)
+  (shell-command-on-region (point-min) (point-max)
+                           "xmllint -format -"
+                           (current-buffer) t
+                           "*Xmllint Error Buffer*" t))
+
 ;;;;;;;;;;;;;;;;
 ;; TYPESCRIPT ;;
 ;;;;;;;;;;;;;;;;
