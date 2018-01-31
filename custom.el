85742aa2e7597710f1097e27eeedcd4d96981b68
Arthur Léothaud <hero.in.no.man@gmail.com>
2018-01-31 16:46:33 +0100

    replace quotes with ' in custom file

--- custom.el
+++ custom.el
@@ -4,26 +4,21 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(avy-keys
-   (quote
-    (97 117 105 101 98 233 112 111 116 115 114 110 109 118 100 108 106 99 113 103 104 102)))
+   '(97 117 105 101 98 233 112 111 116 115 114 110 109 118 100 108 106 99 113 103 104 102))
  '(custom-safe-themes
-   (quote
-    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+   '("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))
  '(ecb-options-version "2.40")
  '(elfeed-feeds my-private-feeds)
- '(helm-external-programs-associations (quote (("mp3" . "audacious"))))
+ '(helm-external-programs-associations '(("mp3" . "audacious")))
  '(magit-branch-arguments nil)
- '(magit-fetch-arguments (quote ("--prune")))
- '(magit-log-arguments
-   (quote
-    ("--graph" "--color" "--decorate" "--date-order" "-n100")))
- '(magit-stash-arguments (quote ("--include-untracked")))
+ '(magit-fetch-arguments '("--prune"))
+ '(magit-log-arguments '("--graph" "--color" "--decorate" "--date-order" "-n100"))
+ '(magit-stash-arguments '("--include-untracked"))
  '(org-agenda-files nil)
  '(package-selected-packages
-   (quote
-    (dired-collapse shell-pop highlight-symbol ace-jump-helm-line ace-window aggressive-indent all-the-icons anzu autodisass-java-bytecode avandu better-defaults bool-flip browse-kill-ring butler calfw calfw-cal calfw-gcal calfw-howm calfw-ical calfw-org char-menu color-theme crontab crux define-word describe-number dired+ dired-narrow dirtree dot-mode dumb-jump edit-server elfeed elisp-slime-nav elpy engine-mode expand-region f3 fancy-battery fancy-narrow flycheck git-gutter git-gutter+ git-gutter-fringe git-messenger git-timemachine gitconfig-mode gitignore-mode god-mode google-c-style goto-last-change gradle-mode groovy-mode headlong helm-ag helm-dash helm-descbinds helm-git helm-git-grep helm-lobsters helm-ls-git helm-projectile helm-spotify-plus helm-swoop helpful highlight-indent-guides highlight-symbol htmlize hydra ibuffer-projectile ibuffer-vc idle-highlight-mode imenu-anywhere isearch-dabbrev jade java-snippets jedi js-comint js2-mode magit markdown-mode meghanada miniedit minimap move-text mu4e multiple-cursors neo-tree neotree no-littering nov ob-restclient ob-shell org org-bullets origami persistent-scratch pfuture pg rainbow-blocks rainbow-delimiters rainbow-mode realgud redtick region-bindings-mode rest-client restclient sass-mode shrink-whitespace smart-comment smart-mode-line smartparens smartscan sml-modeline spaceline spaceline-all-the-icons spray sublimity swagger-to-org swiper-helm temporary-persistent tide tidy tomatinho undo-tree use-package-chords validate visual-regexp-steroids volatile-highlights web-beautify web-mode wgrep which-key wttrin yaml-mode zoom-frm zop-to-char)))
- '(sml/use-projectile-p (quote before-prefixes))
- '(tramp-syntax (quote default) nil (tramp)))
+   '(dired-collapse shell-pop highlight-symbol ace-jump-helm-line ace-window aggressive-indent all-the-icons anzu autodisass-java-bytecode avandu better-defaults bool-flip browse-kill-ring butler calfw calfw-cal calfw-gcal calfw-howm calfw-ical calfw-org char-menu color-theme crontab crux define-word describe-number dired+ dired-narrow dirtree dot-mode dumb-jump edit-server elfeed elisp-slime-nav elpy engine-mode expand-region f3 fancy-battery fancy-narrow flycheck git-gutter git-gutter+ git-gutter-fringe git-messenger git-timemachine gitconfig-mode gitignore-mode god-mode google-c-style goto-last-change gradle-mode groovy-mode headlong helm-ag helm-dash helm-descbinds helm-git helm-git-grep helm-lobsters helm-ls-git helm-projectile helm-spotify-plus helm-swoop helpful highlight-indent-guides highlight-symbol htmlize hydra ibuffer-projectile ibuffer-vc idle-highlight-mode imenu-anywhere isearch-dabbrev jade java-snippets jedi js-comint js2-mode magit markdown-mode meghanada miniedit minimap move-text mu4e multiple-cursors neo-tree neotree no-littering nov ob-restclient ob-shell org org-bullets origami persistent-scratch pfuture pg rainbow-blocks rainbow-delimiters rainbow-mode realgud redtick region-bindings-mode rest-client restclient sass-mode shrink-whitespace smart-comment smart-mode-line smartparens smartscan sml-modeline spaceline spaceline-all-the-icons spray sublimity swagger-to-org swiper-helm temporary-persistent tide tidy tomatinho undo-tree use-package-chords validate visual-regexp-steroids volatile-highlights web-beautify web-mode wgrep which-key wttrin yaml-mode zoom-frm zop-to-char))
+ '(sml/use-projectile-p 'before-prefixes)
+ '(tramp-syntax 'default nil (tramp)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
