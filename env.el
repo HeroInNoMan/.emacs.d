b4d321eb4f5f0a58a62a60a6f4266675d0cefc97
Arthur Léothaud <arthur.leothaud@viseo.com>
2018-06-13 16:58:36 +0200

    refacto project layout

--- env.el
+++ env.el
@@ -64,10 +64,10 @@
             jenkins-viewname "<viewname>"))
 
     ;; let access projects quickly in a convenient layout
-    (defhydra hydra-projects(:color teal :columns 3)
-      "projects"
-      ("e" (lambda () (interactive)(ale/open-project user-emacs-directory)) "emacs")
-      ("q" nil "cancel"))
+    (defhydra hydra-projects(:color teal)
+      ("q" nil "cancel" :column "Projets")
+      ("e" (lambda () (interactive)(ale/open-project user-emacs-directory)) "emacs" :column "Perso")
+      ("E" (lambda () (interactive)(ale/open-project "~/outils/emacs")) "emacs source"))
     (global-set-key (kbd "<f9>") 'hydra-projects/body)
     (global-set-key (kbd "C-c C-j") 'hydra-projects/body)))
 
