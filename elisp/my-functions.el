3e5edb6ccaf67fecfc63643f41f763310c39ee99
Arthur Léothaud <arthur.leothaud@enercoop.org>
2019-07-22 18:59:20 +0200

    update sql configuration

--- elisp/my-functions.el
+++ elisp/my-functions.el
@@ -48,34 +48,6 @@ Show the full path file name in the minibuffer."
     (kill-new filename))
   (message (buffer-file-name)))
 
-;; sql related functions
-(defvar sql-last-prompt-pos 1
-  "Position of last prompt when added recording started.")
-(make-variable-buffer-local 'sql-last-prompt-pos)
-(put 'sql-last-prompt-pos 'permanent-local t)
-
-(defun sql-add-newline-first (output)
-  "Add newline to beginning of OUTPUT for `comint-preoutput-filter-functions'.
-This fixes up the display of queries sent to the inferior buffer programatically."
-  (let ((begin-of-prompt
-         (or (and comint-last-prompt-overlay
-                  ;; sometimes this overlay is not on prompt
-                  (save-excursion
-                    (goto-char (overlay-start comint-last-prompt-overlay))
-                    (looking-at-p comint-prompt-regexp)
-                    (point)))
-             1)))
-    (if (> begin-of-prompt sql-last-prompt-pos)
-        (progn
-          (setq sql-last-prompt-pos begin-of-prompt)
-          (concat "\n" output))
-      output)))
-
-(defun sqli-add-hooks ()
-  "Add hooks to `sql-interactive-mode-hook'."
-  (add-hook 'comint-preoutput-filter-functions
-            'sql-add-newline-first))
-
 (defun eval-and-replace ()
   "Replace the preceding sexp with its value."
   (interactive)
