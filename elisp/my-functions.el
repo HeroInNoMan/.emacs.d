18e309866ac8818f593d3fb7d4172f735032a597
Arthur Léothaud <arthur.leothaud@enercoop.org>
2019-07-23 10:13:25 +0200

    format package my-functions.el properly

--- elisp/my-functions.el
+++ elisp/my-functions.el
@@ -1,6 +1,34 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                  Custom lisp functions                     ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; my-functions.el --- summary -*- lexical-binding: t -*-
+
+;; Author: Arthur Léothaud
+;; Maintainer: Arthur Léothaud
+;; Version: version
+;; Package-Requires: (dependencies)
+;; Homepage: homepage
+;; Keywords: keywords
+
+
+;; This file is not part of GNU Emacs
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; my custom lips functions
+
+;;; Code:
 
 (defun ale/revert-buffer-no-confirm ()
   ;; Source: http://www.emacswiki.org/emacs-en/download/misc-cmds.el
