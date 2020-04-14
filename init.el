77e80586b0b399847feab8e1b53c503ca3997e0f
Arthur Léothaud <hero.in.no.man@gmail.com>
2020-04-14 18:19:39 +0200

    parameterize base directory

--- init.el
+++ init.el
@@ -4,5 +4,8 @@
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
-
-(org-babel-load-file (expand-file-name "emacs.org" user-emacs-directory))
+;; (setq base-emacs-directory user-emacs-directory)
+(setq base-emacs-directory "~/Terminalcity/.emacs.d/")
+(add-to-list 'load-path base-emacs-directory)
+(add-to-list 'load-path (expand-file-name "elisp" base-emacs-directory))
+(org-babel-load-file (expand-file-name "emacs.org" base-emacs-directory))
