a1f03738b7a637ae46820eec24b3f60883ea36c6
Arthur Léothaud <hero.in.no.man@gmail.com>
2020-04-15 18:51:11 +0200

    fix ctrlf face (for doom theme)

--- custom.el
+++ custom.el
@@ -12,6 +12,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ctrlf-highlight-active ((t (:inherit isearch :background "dark orange"))))
  '(fringe ((t (:background "black"))))
  '(highlight-indent-guides-character-face ((t (:foreground "gray15"))))
  '(hl-line ((t (:background "grey7"))))
