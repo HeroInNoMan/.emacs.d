a3d9a026a5933f9160f9f64bfafb659d18b77449
Arthur Léothaud <hero.in.no.man@gmail.com>
2020-05-04 14:13:06 +0200

    move functions to my-functions.el

--- elisp/my-functions.el
+++ elisp/my-functions.el
@@ -501,5 +501,26 @@ Swap buffers in the process"
     (unless (search-forward date nil t nil)
       (search-backward date))))
 
+(defun increment-number-decimal (&optional arg)
+  "Increment the number forward from point by ARG."
+  (interactive "p*")
+  (save-excursion
+    (save-match-data
+      (let (inc-by field-width answer)
+        (setq inc-by (if arg arg 1))
+        (skip-chars-backward "0123456789")
+        (when (re-search-forward "[0-9]+" nil t)
+          (setq field-width (- (match-end 0) (match-beginning 0)))
+          (setq answer (+ (string-to-number (match-string 0) 10) inc-by))
+          (when (< answer 0)
+            (setq answer (+ (expt 10 field-width) answer)))
+          (replace-match (format (concat "%0" (int-to-string field-width) "d")
+                                 answer)))))))
+
+(defun decrement-number-decimal (&optional arg)
+  "Decrement the number forward from point by ARG."
+  (interactive "p*")
+  (increment-number-decimal (if arg (- arg) -1)))
+
 (provide 'my-functions)
 ;;; my-functions.el ends here
