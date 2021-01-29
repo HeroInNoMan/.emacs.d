34476f86d4ec3271cba77565799d9e41f341c6af
Arthur Léothaud <arthur@leothaud.eu>
2021-01-29 19:00:37 +0100

    configure smerge-mode

--- custom.el
+++ custom.el
@@ -3,10 +3,30 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-agenda-files
+   '("~/Nextcloud/org/arthur.org" "~/Nextcloud/org/cycliques.org")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(ctrlf-highlight-active ((t (:inherit isearch :background "dark orange"))))
+ '(highlight-indent-guides-character-face ((t (:foreground "gray15"))))
+ '(hl-line ((t (:background "grey7"))))
+ '(idle-highlight ((t (:background "gray10"))))
+ '(ledger-font-xact-highlight-face ((t (:background "gray12"))))
+ '(magit-branch-current ((t (:foreground "#5cEfFF" :box (:line-width (3 . 3) :color "#5cEfFF" :style released-button)))))
+ '(mu4e-flagged-face ((t (:foreground "gold"))))
+ '(org-done ((t (:foreground "PaleGreen" :weight bold))))
+ '(org-headline-done ((((class color) (min-colors 16) (background dark)) (:foreground "dim grey" :strike-through t))))
+ '(phi-search-selection-face ((t (:inherit isearch :background "dark orange"))))
+ '(rainbow-delimiters-depth-1-face ((t (:inherit rainbow-delimiters-base-face :foreground "white smoke"))))
+ '(rainbow-delimiters-depth-2-face ((t (:inherit rainbow-delimiters-base-face :foreground "gold"))))
+ '(rainbow-delimiters-depth-3-face ((t (:inherit rainbow-delimiters-base-face :foreground "lawn green"))))
+ '(rainbow-delimiters-depth-4-face ((t (:inherit rainbow-delimiters-base-face :foreground "dark turquoise"))))
+ '(rainbow-delimiters-depth-5-face ((t (:inherit rainbow-delimiters-base-face :foreground "deep pink"))))
+ '(rainbow-delimiters-depth-6-face ((t (:inherit rainbow-delimiters-base-face :foreground "yellow1"))))
+ '(rainbow-delimiters-depth-7-face ((t (:inherit rainbow-delimiters-base-face :foreground "orange"))))
+ '(rainbow-delimiters-depth-8-face ((t (:inherit rainbow-delimiters-base-face :foreground "white"))))
+ '(rainbow-delimiters-depth-9-face ((t (:inherit rainbow-delimiters-base-face :foreground "dark salmon"))))
+ '(show-paren-match ((t (:underline "white" :weight ultra-bold :background unspecified :foreground unspecified)))))
