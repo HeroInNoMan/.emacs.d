395d423e6749fd39edaf75ab02be4621847f1f96
Arthur Léothaud <arthur@leothaud.eu>
2021-02-14 16:41:22 +0100

    change license

--- elisp/my-functions.el
+++ elisp/my-functions.el
@@ -10,19 +10,10 @@
 
 ;; This file is not part of GNU Emacs
 
-;; This file is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
-;; any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; For a full copy of the GNU General Public License
-;; see <http://www.gnu.org/licenses/>.
-
+;; This project is libre, and licenced under the terms of the
+;; DO WHAT THE FUCK YOU WANT TO PUBLIC LICENCE, version 3.1,
+;; as published by dtf on July 2019. See the COPYING file or
+;; https://ph.dtf.wtf/w/wtfpl/#version-3-1 for more details.
 
 ;;; Commentary:
 
