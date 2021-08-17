d312197aaaeef21737dafac029515f4e6b56c7f3
Arthur Léothaud <hero.in.no.man@gmail.com>
2021-08-17 23:05:31 +0200

    [straight] replace package.el with straight.el

---
+++ early-init.el
@@ -0,0 +1,2 @@
+;; prevent loading of package.el, as straight.el is used
+(setq package-enable-at-startup nil)
--- init.el
+++ init.el
@@ -2,7 +2,7 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-(package-initialize)
+;; (package-initialize)
 
 (setq base-emacs-directory user-emacs-directory)
 (setq config-file (expand-file-name "emacs.el" base-emacs-directory))
