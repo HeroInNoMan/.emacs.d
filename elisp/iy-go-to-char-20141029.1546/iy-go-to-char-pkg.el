1b04f6cc00543dd63c8d7add32381c3023b7feeb
Arthur Léothaud <hero.in.no.man@gmail.com>
2021-08-18 15:42:00 +0200

    [straight] remove manual install of iy-go-to-char

--- elisp/iy-go-to-char-20141029.1546/iy-go-to-char-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from iy-go-to-char.el  -*- no-byte-compile: t -*-
-(define-package "iy-go-to-char" "20141029.1546" "Go to next CHAR which is similar to \"f\" and \"t\" in vim" 'nil :commit "04ab4f5f3a241cbbc9b8c178a22b412a62f632f9" :keywords '("navigation" "search") :authors '(("Ian Yang <doit dot ian (at) gmail dot com>")) :maintainer '("Ian Yang <doit dot ian (at) gmail dot com>") :url "https://github.com/doitian/iy-go-to-char")
