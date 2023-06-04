c9487b41a606eed3118ec6915d3cabac92084cce
Arthur Léothaud <arthur.leothaud@enercoop.org>
2023-06-05 00:15:50 +0200

    recent

--- elisp/my-functions.el
+++ elisp/my-functions.el
@@ -470,6 +470,24 @@ Also make frame fullscreen. Otherwise, open a new scratch
   (interactive)
   (insert (format-time-string "<%F %a %H:%M>")))
 
+(defun cmp-date-property (prop)
+  "Compare two `org-mode' agenda entries, `A' and `B', by some date property.
+
+If a is before b, return -1. If a is after b, return 1. If they
+are equal return nil."
+  (lexical-let ((prop prop))
+    #'(lambda (a b)
+
+        (let* ((a-pos (get-text-property 0 'org-marker a))
+               (b-pos (get-text-property 0 'org-marker b))
+               (a-date (or (org-entry-get a-pos prop)
+                           (format "<%s>" (org-read-date t nil "now"))))
+               (b-date (or (org-entry-get b-pos prop)
+                           (format "<%s>" (org-read-date t nil "now"))))
+               (cmp (compare-strings a-date nil nil b-date nil nil))
+               )
+          (if (eq cmp t) nil (signum cmp))))))
+
 (defun ale/apply-local-theme-modern ()
   "Apply locally-defined modern theme."
   (when (featurep 'color-theme-modern)
