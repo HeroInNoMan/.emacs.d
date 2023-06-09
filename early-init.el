22c33f716ccc5b88ca95f7464aeabad6940afb7a
Arthur Léothaud <arthur@leothaud.eu>
2023-06-09 17:45:51 +0200

    [WIP][package management] move from straight to elpaca

--- early-init.el
+++ early-init.el
@@ -1,2 +1,9 @@
-;; prevent loading of package.el, as straight.el is used
+;; Example Elpaca early-init.el -*- lexical-binding: t; -*-
+
 (setq package-enable-at-startup nil)
+
+;; Local Variables:
+;; no-byte-compile: t
+;; no-native-compile: t
+;; no-update-autoloads: t
+;; End:
