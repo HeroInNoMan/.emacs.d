313b42bac546c466f41a85e5d819df0a65a0d206
Arthur Léothaud <arthur.leothaud@enercoop.org>
2024-08-28 15:44:28 +0200

    [NO PUSH] config docker-compose

--- etc/transient/values.el
+++ etc/transient/values.el
@@ -1,2 +1,5 @@
-((magit-fetch "--prune" "--tags")
+((docker-compose "--project-directory ~/projets/Coopener-v5/devenv/" "--file ~/projets/Coopener-v5/devenv/docker-compose.yml")
+ (docker-compose-logs "--follow")
+ (docker-container-logs "-f" "--tail 1000")
+ (magit-fetch "--prune" "--tags")
  (magit-log:magit-log-mode "-n100" "--date-order" "--graph" "--color" "--decorate"))
